1665a825f9aa41a76a4fbb465a70b62e331d87c0
Conner Charlebois <cdcharlebois@gmail.com>
2022-12-07 09:59:52 -0500

    add widget properties

--- src/RNVectorIcon.jsx
+++ src/RNVectorIcon.jsx
@@ -20,23 +20,44 @@ import ZocialIcon from "react-native-vector-icons/Zocial";
 Use https://oblador.github.io/react-native-vector-icons/ to searxh for icons
  */
 
-export function RNVectorIcon({ library, iconName, size, color, style }) {
-    const renderIcon = (library, iconName, size, color) => {
-        switch (library) {
+export function RNVectorIcon({ family, iconName, size, color, style }) {
+    const renderIcon = (family, iconName, size, color) => {
+        const _args = { name: iconName, size, color };
+        switch (family) {
             case "fa":
-                return <FaIcon name={iconName} size={size} color={color} />
+                return <FaIcon {..._args} />;
             case "fa5":
-                return <Fa5Icon name={iconName} size={size} color={color} />
+                return <Fa5Icon {..._args} />;
             case "ant":
-                return <AntIcon name={iconName} size={size} color={color} />
+                return <AntIcon {..._args} />;
+            case "entypo":
+                return <EntypoIcon {..._args} />;
+            case "evil":
+                return <EvilIcon {..._args} />;
+            case "feather":
+                return <FeatherIcon {..._args} />;
+            case "fa5pro":
+                return <Fa5ProIcon {..._args} />;
+            case "fontisto":
+                return <FontistoIcon {..._args} />;
+            case "foundation":
+                return <FoundationIcon {..._args} />;
+            case "ionicon":
+                return <IoniconIcon {..._args} />;
+            case "materialCommunity":
+                return <MaterialCommunityIcon {..._args} />;
+            case "material":
+                return <MaterialIcon {..._args} />;
+            case "octicon":
+                return <OcticonIcon {..._args} />;
+            case "simpleline":
+                return <SimpleLineIcon {..._args} />;
+            case "zocial":
+                return <ZocialIcon {..._args} />;
         }
-    }
-    return (
-        <View>
-            {renderIcon("fa", "rocket", 30, "red")}
-            {renderIcon("fa5", "rocket", 30, "red")}
-            {renderIcon("ant", "upcircleo", 30, "red")}
-        </View>
-    );
-    // return <HelloWorld name={yourName} style={style} />;
+    };
+
+    return iconName.status === "available" && color.status === "available" && size.status === "available" ? (
+        <View>{renderIcon(family, iconName.value, size.value * 1, color.value)}</View>
+    ) : null;
 }
